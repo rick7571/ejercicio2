9b449a1a27dd284b7965dc53f4392270b4b1a7ba
Ricardo <ric75711904@gmail.com>
2021-04-12 15:35:39 -0400

    codigo para trabajar por ramas

---
+++ src/Aritmetica.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Aritmetica {
+    public static void main(String[] args)  {
+        Scanner ent= new Scanner(System.in);
+        double x;
+        double y;
+        double resSuma;
+        double resResta;
+        double resMult;
+        
+        System.out.println("Ingrese primer valor: ");
+        x=ent.nextDouble();
+        System.out.println("Ingrese primer valor: ");
+        y=ent.nextDouble();
+        resSuma=suma(x,y);
+        System.out.println("La suma es: " + resSuma);
+        resResta=resta(x,y);
+        System.out.println("La resta es: " + resResta);
+        resMult=multiplicacion(x,y);
+        System.out.println("La Multiplicacion es: " + resMult);
+    }
+    static double suma(double a, double b)
+    {
+        
+        
+        return 1;
+
+    }
+    static double resta(double a, double b)
+    {
+        
+        
+        return 1;
+
+    }
+    static double multiplicacion(double a, double b)
+    {
+        
+        
+        return 1;
+
+    }
+}
